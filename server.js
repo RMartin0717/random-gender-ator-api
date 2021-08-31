b2531c89363a4abf72fd73ef9c3e83ea3519b26f
Riley Martin <riley.willow@gmail.com>
2021-08-31 15:36:22 -0600

    Create server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+//getting access to express
+const app = express()
+//run express. app is a common variable for server
+
+app.set('port', process.env.PORT || 3001)
+//set the port
+
+app.listen(app.get('port'), () => {
+  //get port (set above) and start server. 'listen' on this port 
+  console.log(`App is now listening on port ${app.get('port')}!`)
+})
