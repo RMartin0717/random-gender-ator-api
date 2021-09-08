bf3188c2d9a6226a2d4c03a4baacfb16d06c1800
Riley Martin <riley.willow@gmail.com>
2021-09-08 09:27:16 -0600

    Edit migration to create extraterrestrials, trashes, and sparkles tables

--- db/migrations/20210901190800_initial.js
+++ db/migrations/20210901190800_initial.js
@@ -9,9 +9,36 @@ exports.up = function(knex) {
 
       table.timestamps(true, true)
     })
+    .createTable('extraterrestrials', function(table) {
+      table.increments('id')
+        .primary()
+      table.string('extraterrestial_association')
+        .notNullable()
+
+      table.timestamps(true, true)
+    })
+    .createTable('trashes', function(table) {
+      table.increments('id')
+        .primary()
+      table.string('trash_association')
+        .notNullable()
+
+      table.timestamps(true, true)
+    })
+    .createTable('sparkles', function(table) {
+      table.increments('id')
+        .primary()
+      table.string('sparkle_association')
+        .notNullable()
+
+      table.timestamps(true, true)
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTable('animals')
+    .dropTable('extraterrestrials')
+    .dropTable('trashes')
+    .dropTable('sparkles')
 };
