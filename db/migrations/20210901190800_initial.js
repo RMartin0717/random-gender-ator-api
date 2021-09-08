3879b6af8c180910b8da4745d6aba03e3c75445b
Riley Martin <riley.willow@gmail.com>
2021-09-08 09:17:55 -0600

    Edit migration to create animal table in exports.up

--- db/migrations/20210901190800_initial.js
+++ db/migrations/20210901190800_initial.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('animals', function(table) {
+      table.increments('id')
+        .primary()
+      table.string('animal_association')
+        .notNullable()
+
+      table.timestamps(true, true)
+    })
 };
 
 exports.down = function(knex) {
-  
+
 };
