b08dda45784e96e56814e0ef183ba2f2eb1ef005
Riley Martin <riley.willow@gmail.com>
2022-02-01 16:28:49 -0700

    Write dynamic routes for GETing, POSTing, DELETEing

--- server.js
+++ server.js
@@ -17,8 +17,51 @@ app.use(cors())
 
 //routes
   //get array of word associations (dynamic path)
+app.get(`/api/v1/:word`, async (request, response) => {
+  const { word } = request.params
+  try {
+    const words = await database([word]).select()
+    response.status(200).json(words)
+  }
+  catch(error) {
+    response.status(500).json({ error})
+  }
+})
+
   //post more words to the arrays
-  //delete words from teh arrays
+app.post(`/api/v1/:word`, async (request, response) => {
+  const { word } = request.params
+  const { wordAssociation } = request.body
+  const newWord = request.body
+
+  if (!wordAssociation) {
+  response.status(422).json({ error: `Missing property of wordAssociation. Expected format: { wordAssociation: <String> }`})
+  }
+  try {
+  const id = await database(`${word}`).insert(newWord, 'id')
+  response.status(201).json({ id });
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+})
+
+  //delete words from the arrays
+app.delete(`/api/v1/:word/:id`, async (request, response) => {
+  const { word, id } = request.params
+  try {
+    const deleteWord = await database(`${word}`).where({ id }).del()
+    if (!deleteWord) {
+      response.status(404).json({ error: `Word not found.`})
+    }
+    response.status(201).json(`${deleteWord} word deleted`)
+  } catch(error) {
+    response.status(500).json(error.message)
+  }
+})
+
+app.get('/', (request, response) => {
+  response.json({ test: `Server is waiting to GET some info!` })
+});
 
 app.listen(port, () => {
   //get port (set above) and start server. 'listen' on this port
